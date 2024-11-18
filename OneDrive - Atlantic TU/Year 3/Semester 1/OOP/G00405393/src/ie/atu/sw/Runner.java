2ec050fbef5c969c296d64668c5dba2b0f921d11
trinv1 <trinityvillaruel@gmail.com>
2024-11-18 17:39:15 +0000

    Made Menu class that performs action based on user choice
    Moved menu from runner class to menu class and added to ot
    Menu class so far allows user to load embeddings file, create output
    file or exit

--- OneDrive - Atlantic TU/Year 3/Semester 1/OOP/G00405393/src/ie/atu/sw/Runner.java
+++ OneDrive - Atlantic TU/Year 3/Semester 1/OOP/G00405393/src/ie/atu/sw/Runner.java
@@ -2,7 +2,8 @@ package ie.atu.sw;
 
 /**
  * The Runner class is the entry point for the Text Simplifier project
- * It initializes the menu face, displays options to user and holds a progress meter
+ * It initializes the menu and calls the start method to begin the program
+ * It holds and calls a method to a display progress meter
  *
  * @author Trin Villaruel
  * @version 4.29
@@ -10,97 +11,68 @@ package ie.atu.sw;
  */
 
 public class Runner {
-	
-	 /**
-     * The main method that starts the program.
-     * It displays the menu options and includes a progress meter demonstration.
-     *
-     * @param args the command-line arguments
-     * @throws Exception if there is an error during execution
-     */
-	public static void main(String[] args) throws Exception {
-		//You should put the following code into a menu or Menu class
-		System.out.println(ConsoleColour.WHITE);
-		System.out.println("************************************************************");
-		System.out.println("*     ATU - Dept. of Computer Science & Applied Physics    *");
-		System.out.println("*                                                          *");
-		System.out.println("*             Virtual Threaded Text Simplifier             *");
-		System.out.println("*                                                          *");
-		System.out.println("************************************************************");
-		System.out.println("(1) Specify Embeddings File");
-		System.out.println("(2) Specify Google 1000 File");
-		System.out.println("(3) Specify an Output File (default: ./out.txt)");
-		System.out.println("(4) Execute, Analyse and Report");
-		System.out.println("(5) Optional Extras...");
-		System.out.println("(?) Quit");
-		
-		//Output a menu of options and solicit text from the user
-		System.out.print(ConsoleColour.BLACK_BOLD_BRIGHT);
-		System.out.print("Select Option [1-4]>");
-		System.out.println();
-		
-		
-		//You may want to include a progress meter in you assignment!
-		System.out.print(ConsoleColour.YELLOW);	//Change the colour of the console text
-		int size = 100;							//The size of the meter. 100 equates to 100%
-		for (int i =0 ; i < size ; i++) {		//The loop equates to a sequence of processing steps
-			printProgress(i + 1, size); 		//After each (some) steps, update the progress meter
-			Thread.sleep(10);					//Slows things down so the animation is visible 
-		}
-	}
-	
 
-	
-	/*
-	 *  Terminal Progress Meter
-	 *  
-	 *  1) The progress meter will NOT work in the Eclipse console, but will
-	 *     work on Windows (DOS), Mac and Linux terminals.
-	 *     
-	 *  2) The meter works by using the line feed character "\r" to return to
-	 *     the start of the current line and writes out the updated progress
-	 *     over the existing information. If you output any text between 
-	 *     calling this method, i.e. System.out.println(....), then the next
-	 *     call to the progress meter will output the status to the next line.
-	 *      
-	 *  3) If the variable size is greater than the terminal width, a new line
-	 *     escape character "\n" will be automatically added and the meter won't
-	 *     work properly.  
-	 *  
-	 * 
+    public static void main(String[] args) {
+        try {
+            System.out.println("Welcome to your Virtual Thread Simplifier!");
+
+            Menu menu = new Menu();//Initializing the menu 
+            menu.start();
+            
+            displayProgressMeter(); //Displaying the progress meter 
+
+
+        } catch (Exception e) {
+            System.out.println("An error occurred: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * Method to display a progress meter
+     * Running Time: 0(n)
+     * Method iterates over a loop w/ "n" iterations where n's the value of the size variable
+     * 
+     */
+    public static void displayProgressMeter() {// Method displaying the progress meter
+    	
+    	System.out.print(ConsoleColour.GREEN);   
+        int size = 100;                         
+        for (int i = 0; i < size; i++) {        
+            printProgress(i + 1, size);         
+            try {
+                Thread.sleep(10);               
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+   	/**
+	 * Method to gradually print progress
+	 * Running time: 0(1)
+	 * Method does a series of constant time operations regardless of input size
+	 * It doesnt have any loops and all operations in it execute in constant time
 	 */
-	public static void printProgress(int index, int total) {
-		if (index > total) return;	//Out of range
-        int size = 50; 				//Must be less than console width
-	    char done = '#';			//Change to whatever you like.
-	    char todo = '-';			//Change to whatever you like.
-	    
-	    //Compute basic metrics for the meter
+    public static void printProgress(int index, int total) {
+    	
+        if (index > total) return;    
+        int size = 50;                
+        char done = 'Y';          
+        char todo = 'D';            
+
         int complete = (100 * index) / total;
         int completeLen = size * complete / 100;
-        
-        /*
-         * A StringBuilder should be used for string concatenation inside a 
-         * loop. However, as the number of loop iterations is small, using
-         * the "+" operator may be more efficient as the instructions can
-         * be optimized by the compiler. Either way, the performance overhead
-         * will be marginal.  
-         */
+
         StringBuilder sb = new StringBuilder();
         sb.append("[");
         for (int i = 0; i < size; i++) {
-        	sb.append((i < completeLen) ? done : todo);
+            sb.append((i < completeLen) ? done : todo);
         }
-        
-        /*
-         * The line feed escape character "\r" returns the cursor to the 
-         * start of the current line. Calling print(...) overwrites the
-         * existing line and creates the illusion of an animation.
-         */
-        System.out.print("\r" + sb + "] " + complete + "%");
-        
-        //Once the meter reaches its max, move to a new line.
-        if (done == total) System.out.println("\n");
+
+        System.out.print("\r" + sb + "] " + complete + "%");//Printing the progress bar
+
+        if (complete == 100) System.out.println();//once the progress reaches its max, moving to a new line
     }
-	
 }
